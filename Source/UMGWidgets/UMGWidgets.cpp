70894e8215af2399991d872f41d9473f59ecb8af
DenisGonchar <denisgonchar123@gmail.com>
2024-01-16 13:42:05 +0300

    Initial commit

---
+++ Source/UMGWidgets/UMGWidgets.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UMGWidgets.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UMGWidgets, "UMGWidgets" );
